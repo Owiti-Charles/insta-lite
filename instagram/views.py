ed0e17aabaca5451bc7551142b5f63ab19e3e65f
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-10 23:33:51 +0300

    Create a loginform and signup form

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def signup(request):
-    return render(request, 'registration/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
