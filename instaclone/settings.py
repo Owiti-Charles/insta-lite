3d6e58bb1e017abb19eeadd39d9ab3c83ed69139
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-12 11:50:06 +0300

    Create profile model

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -111,6 +111,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'index'
 
--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='images/', default='images/default.png')
+    bio = models.TextField(max_length=500, default="Welcome Me!")
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
