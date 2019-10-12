a526eb80c96f1f5ee657d4ca48e8976fa55cbdc3
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-12 10:20:14 +0300

    Add a Navbar to make it easier for a user to navigate through the site

--- instagram/views.py
+++ instagram/views.py
@@ -10,6 +10,10 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
