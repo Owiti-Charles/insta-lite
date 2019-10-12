ef9e40c7ff82119422bfe1a4153b2665cdc35c98
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-12 15:53:54 +0300

    Populate the edit profile forms to allow the user be able to see what was previously saved in the database

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
-    re_path(r'profile/(?P<username>\w+)/', views.profile, name='profile'),
+    path('profile/<username>/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -27,8 +27,16 @@ def index(request):
 
 @login_required(login_url='login')
 def profile(request, username):
-    user_form = UpdateUserForm()
-    prof_form = UpdateUserProfileForm()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.FILES, request.POST, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            redirect('profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
     params = {
         'user_form': user_form,
         'prof_form': prof_form,
