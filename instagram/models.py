14e3356f118104217c13b81afe56544a059a28e4
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-13 17:40:01 +0300

    Create likes model

--- instagram/models.py
+++ instagram/models.py
@@ -47,3 +47,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.user.name} Post'
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='user_likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
