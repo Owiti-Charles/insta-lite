1f06101632ec5c22cd3df06b96832bcc5ee569e5
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-17 13:00:53 +0300

    Edit follow and unfollow buttons

--- instagram/views.py
+++ instagram/views.py
@@ -77,11 +77,19 @@ def user_profile(request, username):
     if request.user == user_prof:
         return redirect('profile', username=request.user.username)
     user_posts = user_prof.profile.posts.all()
+    
     followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
     params = {
         'user_prof': user_prof,
         'user_posts': user_posts,
-        'followers': followers
+        'followers': followers,
+        'follow_status': follow_status
     }
     print(followers)
     return render(request, 'instagram/user_profile.html', params)
